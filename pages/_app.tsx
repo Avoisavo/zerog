98bd15dc92e9e3fed491a128777243bea0bdbd01
JingYuan <jingyuan0926@gmail.com>
2026-04-01 16:33:23 +0200

    Fix bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { wagmiConfig } from "@/lib/wagmi";
 
 const queryClient = new QueryClient();
@@ -10,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <RainbowKitProvider locale="en-US">
+          <Component {...pageProps} />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
